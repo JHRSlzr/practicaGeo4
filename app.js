9693c5e02639696c11f6eaefbc3355f9ece7ac6f
hernan reyes <hernanryz@hotmail.com>
2021-02-27 22:33:04 -0600

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function getUbication() {
 
     function showPosition(pos) {
 
-        var coords = pos.coords.latitude + "," + pos.coords.longitude;
+        var coords = pos.coords.latitude + "||||" + pos.coords.longitude;
 
         var mapimg = document.getElementById("mapimg");
         mapimg.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + coords + "&zoom=18&size=800x800&key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24";
