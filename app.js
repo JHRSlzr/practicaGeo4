5aa01c74cdb65fd46fb059786447b3e34d767762
hernan reyes <hernanryz@hotmail.com>
2021-02-27 22:52:52 -0600

    practica 4

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ function obtieneUbicacion() {
 
         var coordenadas = posicion.coords.latitude + "," + posicion.coords.longitude;
 
-        var imagenmapa = document.getElementById("imagenmapa");
-        imagenmapa.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + coordenadas + "&zoom=18&size=800x800&key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24";
+        var imgmapa = document.getElementById("imgmapa");
+        imgmapa.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + coordenadas + "&zoom=18&size=800x800&key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24";
 
 
     }
